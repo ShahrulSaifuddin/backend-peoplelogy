fa3e055f107b3dc095c85c1e440b114f5f7a7fcd
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-12-16 15:31:16 +0800

    added form and prettier for php

--- Day13-php/tutorial/data.php
+++ Day13-php/tutorial/data.php
@@ -1,28 +1,58 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
- echo "This is our data type";
- echo "<br>";
- $friends = array("john", "anna", "mike");
- echo var_dump($friends);
- echo "<br>";
- echo "$friends[0]";
- echo "<br>";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <title>Document</title>
+</head>
 
- $name = "hello world";
- echo "$name";
- echo "<br>";
- echo "the length is: " . strlen($name);
- echo "<br>";
- echo "total words is: " . str_word_count($name);
- echo "<br>";
- echo "reverse string: " . strrev($name);
+<body>
 
- $age = 30;
+  <?php
+  // define variables and set to empty values
+  $email = $password = "";
 
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = test_input($_POST["email"]);
+    $password = test_input($_POST["pswd"]);
+  }
 
+  function test_input($data)
+  {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+  ?>
 
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <div class="mb-3 mt-3">
+      <label for="email" class="form-label">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="<?php echo $email; ?>">
+    </div>
+    <div class="mb-3">
+      <label for="pwd" class="form-label">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+    </div>
+    <div class="form-check mb-3">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 
+  <?php
+  echo "<h2>Your Input:</h2>";
+  echo $email;
+  echo "<br>";
+  echo $password;
+  ?>
 
+</body>
 
-
-?>
+</html>
