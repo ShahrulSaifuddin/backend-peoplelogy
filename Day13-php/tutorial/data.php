fd4dfbe3ade1739494741df2ac88b306b4c78c56
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-12-16 12:56:23 +0800

    task-2

--- Day13-php/tutorial/data.php
+++ Day13-php/tutorial/data.php
@@ -1,12 +1,12 @@
 <?php
 
- // echo "This is our data type";
- // echo "<br>";
- // $friends = array("john", "anna", "mike");
- // echo var_dump($friends);
- // echo "<br>";
- // echo "$friends[0]";
- // echo "<br>";
+ echo "This is our data type";
+ echo "<br>";
+ $friends = array("john", "anna", "mike");
+ echo var_dump($friends);
+ echo "<br>";
+ echo "$friends[0]";
+ echo "<br>";
 
  $name = "hello world";
  echo "$name";
@@ -15,7 +15,12 @@
  echo "<br>";
  echo "total words is: " . str_word_count($name);
  echo "<br>";
- echo "reverse string: " . strrev($name)
+ echo "reverse string: " . strrev($name);
+
+ $age = 30;
+
+
+
 
 
 
